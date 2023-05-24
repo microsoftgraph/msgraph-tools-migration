56f38a59b2666afcd80019b8b3cdb527e2d53215
Timothy Wamalwa <timwamalwa@gmail.com>
2023-05-24 18:03:25 +0300

    Removed command from function

--- GenerateNuspec.ps1
+++ GenerateNuspec.ps1
@@ -1,5 +1,3 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-function Generate-NuspecFile{
-    nuget spec Microsoft.Graph.Migration.Tool
-}
+nuget spec Microsoft.Graph.Migration.Tool
